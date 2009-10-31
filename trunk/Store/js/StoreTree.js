ed41cf67f7fa9dbbd29f1f144a7415b6cf576605
com.chinaos.snaill <com.chinaos.snaill@346b722a-c5ff-11de-8613-b9ccef3c78c6>
2009-10-31 13:58:56 +0000

    增加JFS，Store，Alumb的代码

---
+++ trunk/Store/js/StoreTree.js
@@ -0,0 +1,67 @@
+﻿/*
+ * Jeebook store 1.0
+ * Copyright(c) 2008, Jeebook.com
+ * snaill@jeebook.com
+ * 
+ * http://www.jeebook.com
+ */
+
+Ext.app.StoreTree = function() {
+	
+	Ext.app.StoreTree.superclass.constructor.call(this, {
+		id			: 'StoreTree_Id',
+	//	title       : 'Folders',
+		region      : 'west',
+		split       : true,
+		useArrows	: true,
+        animate		: true,
+		width       : 200,
+		collapsible : true,
+		collapseMode:'mini',
+		margins     : '3 0 3 3',
+		cmargins    : '3 3 3 3',
+		autoScroll 	: true,
+		loader		: new Ext.app.StoreTreeLoader({
+       		clearOnLoad:false
+       	}),
+		root 		: new Ext.tree.AsyncTreeNode({
+			id:'root',
+			text:'Store'
+		})
+	});
+
+	this.on('click', function( node ) {
+		Ext.app.StoreGrid.getObj().load( this.getPath(node) );
+		
+		var event = {};
+		event.id = Ext.app.Event.FolderChanged;
+		event.node = node;
+		Ext.app.MainPanel.getObj().onNotify( event );
+	}, this );
+	
+	this.root.expand();
+};
+
+Ext.extend(Ext.app.StoreTree, Ext.tree.TreePanel, {
+	getPath : function( node ) {
+		var p = node.getPath() + '/';
+		// delete /root
+		return p.substring( 5 );
+	},
+	getCurrentPath : function(){
+		var node = this.getSelectionModel().getSelectedNode();
+		if ( !node )
+			node = this.root;
+		return this.getPath( node );
+	},
+	refresh : function(){
+		var node = this.getSelectionModel().getSelectedNode();
+		if ( !node )
+			node = this.root;
+		node.reload();
+	}
+} );
+
+Ext.app.StoreTree.getObj = function(){
+	return Ext.getCmp('StoreTree_Id');
+};
