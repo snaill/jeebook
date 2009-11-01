7861a2b61ce39be3a1cd15eda2459b3f8b3c3eee
com.chinaos.snaill <com.chinaos.snaill@346b722a-c5ff-11de-8613-b9ccef3c78c6>
2009-11-01 08:40:20 +0000

    实现Reader读取Comic目录

--- trunk/Reader/ComicView.cs
+++ trunk/Reader/ComicView.cs
@@ -6,25 +6,98 @@ using Jeebook.Base;
 
 namespace Jeebook.Reader
 {
-    class ComicView : System.Windows.Forms.PictureBox
+    public partial class ComicView : System.Windows.Forms.Panel
     {
+        const int ImageMinMove = 20;
+        MediaObject _media = null;
+        Proxy _proxy = null;
+        int _index = 0;
 
-        public ComicView(Book book, Proxy proxy)
+        public ComicView(MediaObject media, Proxy proxy)
         {
+            InitializeComponent();
+
+            _media = media;
+            _proxy = proxy;
+
+            Load(_index);
         }
 
-        private void InitializeComponent()
+        public void Load(int index)
         {
-            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
-            this.SuspendLayout();
-            // 
-            // ComicView
-            // 
-            this.WaitOnLoad = true;
-            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
-            this.ResumeLayout(false);
+            if (index < 0 || index >= _media.Objects.Count)
+                return;
 
+            System.IO.Stream stream = _proxy.GetFileStream(_media.Objects[index].FileRef);
+            ImageBox.Image = System.Drawing.Image.FromStream(stream);
+            stream.Close();
+
+            ResizeImageBox();   
         }
 
+        public void ResizeImageBox()
+        {
+            //
+            if (this.Width < ImageBox.Image.Width)
+            {
+                ImageBox.Height = ImageBox.Image.Height * this.Width / ImageBox.Image.Width;
+                ImageBox.Width = this.Width;
+            }
+
+            ImageBox.Left = (this.Width - ImageBox.Width) / 2;
+            ImageBox.Top = 0;
+        }
+
+        private void ComicView_Resize(object sender, EventArgs e)
+        {
+            ResizeImageBox();
+        }
+
+        private void ComicView_MouseWheel(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            System.Windows.Forms.Keys keyData = e.Delta > 0 ? System.Windows.Forms.Keys.Down : System.Windows.Forms.Keys.Up;
+            System.Windows.Forms.KeyEventArgs args = new System.Windows.Forms.KeyEventArgs( keyData );
+            OnKeyDown( args );
+        }
+        
+        protected override void OnKeyDown(System.Windows.Forms.KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case System.Windows.Forms.Keys.Left:
+                    {
+                        if (_index == 0)
+                            return;
+
+                        Load(--_index);
+                        break;
+                    }
+                case System.Windows.Forms.Keys.Right:
+                    {
+                        if (_index == _media.Objects.Count - 1)
+                            return;
+
+                        Load(++_index);
+                        break;
+                    }
+                case System.Windows.Forms.Keys.Up:
+                    {
+                        if (ImageBox.Top >= 0)
+                            return;
+
+                        ImageBox.Top += (ImageMinMove < -ImageBox.Top) ? ImageMinMove : -ImageBox.Top;
+                        break;
+                    }
+                case System.Windows.Forms.Keys.Down:
+                    {
+                        if (ImageBox.Bottom <= this.Height)
+                            return;
+
+                        ImageBox.Top -= (ImageMinMove < ImageBox.Bottom - this.Height) ? ImageMinMove : ImageBox.Bottom - this.Height;
+                        break;
+                    }
+            }
+            base.OnKeyDown(e);
+        }
     }
 }
