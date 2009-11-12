4dfc65bb5a25dfe1d10406082e1ca67cbac5bf7c
com.chinaos.snaill <com.chinaos.snaill@346b722a-c5ff-11de-8613-b9ccef3c78c6>
2009-11-12 16:29:28 +0000

    增加事件处理不同应用的cache目录 实现HttpProxy，未测试

--- trunk/Base/HttpProxy.cs
+++ trunk/Base/HttpProxy.cs
@@ -2,19 +2,68 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Collections.Specialized;
+using System.Text.RegularExpressions;
 
 namespace Jeebook.Base
 {
     public class HttpProxy : Proxy
     {
+        string _uri = "";
         public HttpProxy(string uri)
         {
-
+            _uri = ParseUrl(uri);
         }
 
         public System.IO.Stream GetFileStream(string fn)
         {
-            return null;
+            System.Net.WebRequest request = System.Net.HttpWebRequest.Create(_uri + "#" + fn);
+            System.Net.WebResponse response = request.GetResponse();
+            return response.GetResponseStream();
+        }
+        
+        string ParseUrl(string url)
+        {
+            if (url == null)
+                throw new ArgumentNullException("url");
+
+            NameValueCollection nvc = new NameValueCollection();
+            string baseUrl = "";
+
+            if (url == "")
+                return "";
+
+            int questionMarkIndex = url.IndexOf('?');
+
+            if (questionMarkIndex == -1)
+            {
+                baseUrl = url;
+                return "";
+            }
+            baseUrl = url.Substring(0, questionMarkIndex);
+            if (questionMarkIndex == url.Length - 1)
+                return ""; 
+            string ps = url.Substring(questionMarkIndex + 1);
+
+            // 开始分析参数对    
+            Regex re = new Regex(@"(^|&)?(\w+)=([^&]+)(&|$)?", RegexOptions.Compiled);
+            MatchCollection mc = re.Matches(ps);
+
+            foreach (Match m in mc)
+            {
+                nvc.Add(m.Result("$2"), m.Result("$3"));
+            }
+
+            string strUrl = baseUrl + "?";
+            foreach( string key in nvc.Keys )
+            {
+                if (key == "path")
+                    continue;
+
+                strUrl += key + "=" + nvc[key] + '&';
+            }
+            strUrl += "path=" + nvc["path"];
+            return strUrl;
         }
     }
 }
