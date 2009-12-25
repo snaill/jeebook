933fb79384370347e39de374a8710588fc3624e2
com.chinaos.snaill <com.chinaos.snaill@346b722a-c5ff-11de-8613-b9ccef3c78c6>
2009-12-25 10:58:45 +0000

    增加BaseHandler基类

---
+++ trunk/FileServer/BaseHandler.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace Jeebook.ROA
+{
+    class BaseHandler : IHttpHandler
+    {
+        public void ProcessRequest(HttpContext context)
+        {
+            if (context.Request.HttpMethod == "GET")
+                do_Get(context);
+            else if (context.Request.HttpMethod == "PUT")
+                do_Put(context);
+            else if (context.Request.HttpMethod == "DELETE")
+                do_Delete(context);
+            else if (context.Request.HttpMethod == "POST")
+            {
+                string strMethod = context.Request.QueryString["method"];
+                if ( strMethod == "PUT" )
+                    do_Put(context);
+                else if ( strMethod == "DELTE" )
+                    do_Delete(context);
+                else
+                    do_Post(context);
+            }
+        }
+
+        public virtual void do_Get(HttpContext context)
+        {
+            context.Response.StatusCode = HttpStatusCode.HTTP_501_NotImplemented;
+        }
+
+        public virtual void do_Post(HttpContext context)
+        {
+            context.Response.StatusCode = HttpStatusCode.HTTP_501_NotImplemented;
+        }
+
+        public virtual void do_Put(HttpContext context)
+        {
+            context.Response.StatusCode = HttpStatusCode.HTTP_501_NotImplemented;
+        }
+
+        public virtual void do_Delete(HttpContext context)
+        {
+            context.Response.StatusCode = HttpStatusCode.HTTP_501_NotImplemented;
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
+
--- trunk/FileServer/StoreCategory.cs
+++ trunk/FileServer/StoreCategory.cs
@@ -4,34 +4,16 @@ using System.Linq;
 using System.Text;
 using Jeebook.FileServer;
 using System.Web;
+using Jeebook.ROA;
 
 namespace Jeebook.Store.ROA
 {
-    class Category : IHttpHandler
+    class Category : BaseHandler
     {
-        public void ProcessRequest(HttpContext context)
+        public override void do_Get(HttpContext context)
         {
             string strPath = context.Request.QueryString["path"];
 
-            try
-            {
-                if (context.Request.HttpMethod == "GET")
-                    do_Get(context, strPath);
-                else if (context.Request.HttpMethod == "POST")
-                    do_Post(context, strPath);
-                else if (context.Request.HttpMethod == "PUT")
-                    do_Put(context, strPath);
-                else if (context.Request.HttpMethod == "DELETE")
-                    do_Delete(context, strPath);
-            }
-            catch (FileServerException)
-            {
-
-            }
-        }
-
-        public void do_Get(HttpContext context, string strPath)
-        {
             //
             FileServerBase fs = new FileServerBase(context.Server.MapPath("../data/"));
             string dirs = fs.GetDirectories(strPath);
@@ -39,25 +21,17 @@ namespace Jeebook.Store.ROA
             context.Response.WriteFile(dirs);
         }
 
-        public void do_Post(HttpContext context, string strPath)
-        {
+        //public void do_Post(HttpContext context)
+        //{
             
-        }
+        //}
 
-        public void do_Put(HttpContext context, string strPath)
-        {
-        }
+        //public void do_Put(HttpContext context)
+        //{
+        //}
 
-        public void do_Delete(HttpContext context, string strPath)
-        {
-        }
-
-        public bool IsReusable
-        {
-            get
-            {
-                return false;
-            }
-        }
+        //public void do_Delete(HttpContext context)
+        //{
+        //}
     }
 }
