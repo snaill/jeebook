49e54f7dcbe07426b7d5fa7a74497117dd297196
com.chinaos.snaill <com.chinaos.snaill@346b722a-c5ff-11de-8613-b9ccef3c78c6>
2010-01-12 16:07:34 +0000

    添加Store2，使用JQuery构建

---
+++ trunk/Store2/js/Store.js
@@ -0,0 +1,16 @@
+﻿/*
+ * Jeebook store 2.0
+ * Copyright(c) 2008-2009, Jeebook.com
+ * snaill@jeebook.com
+ * 
+ * http://www.jeebook.com
+ */
+
+$(document).ready(function(){
+
+	// end init
+	setTimeout(function(){
+		$('#loading').remove();
+		$('#loading-mask').fadeOut({remove:true});
+	}, 250);
+});
