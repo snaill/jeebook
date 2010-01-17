8c11ef559907553e9c556f4ba1107459ee8294a5
com.chinaos.snaill <com.chinaos.snaill@346b722a-c5ff-11de-8613-b9ccef3c78c6>
2010-01-17 11:23:53 +0000

    添加API，实现获取Paging数据 添加msgbar插件 添加上传按钮 添加Store2 sln文件 修改Commandbar实现

--- Store2/js/Store.js
+++ Store2/js/Store.js
@@ -7,7 +7,84 @@
  */
 
 $(document).ready(function(){
+	
+	$("div#cmdbar").corner("tl bl 24px");
+	
+	$("div#msgbar").msgbar({msg:'test'});
 
+/*	$("div#cmdbar").shadow({
+           width:5, 
+           startOpacity:60, 
+           endOpacity:10, 
+           cornerHeight:8, 
+           color:"#000000"
+    });
+*/
+	var button = $('#uploadbtn'), interval;
+	
+	new AjaxUpload(button, {
+		action: 'api/book/upload/', 
+	//	name: 'myfile',
+		onSubmit : function(file, ext){
+			// Allow only images. You should add security check on the server-side.
+			if (!(ext && /^(bmp|png|jpeg|gif)$/.test(ext))){
+				// extension is not allowed
+				$('#example2 .text').text('Error: only images are allowed');
+				// cancel upload
+				return false;				
+			}	
+			
+			// If you want to allow uploading only 1 file at time,
+			// you can disable upload button
+			this.disable();
+			
+			// Uploding -> Uploading. -> Uploading...
+			interval = window.setInterval(function(){
+				var text = button.text();
+				if (text.length < 5){
+					button.text(text + '.');					
+				} else {
+					button.text('');				
+				}
+			}, 200);
+		},
+		onComplete: function(file, response){
+			button.text('Upload');
+						
+			window.clearInterval(interval);
+						
+			// enable upload button
+			this.enable();
+			
+			// add file to the list
+			$('<li></li>').appendTo('#example1 .files').text(file);						
+		}
+	});
+		
+	// init pagination
+	$.ajax({
+		url 	: 'api/category/upload/',
+		type	: "GET",
+		async	: true,
+		dataType : 'json',
+		success : function(data){
+			var labels = [];
+			for ( var i = 0; i < data.length; i ++ )
+				labels[i] = data[i].name;
+			
+			$("div#pagination").pagination(data.length, {
+				num_edge_entries: 2,
+				num_display_entries: 8,
+				callback: function(page_index, jq){
+				},
+				items_per_page:1
+			});
+		},
+		error	: function(xhr){
+			alert('error');
+		}
+	});
+			
 	// end init
 	setTimeout(function(){
 		$('#loading').remove();
---
+++ Store2/js/jquery.msgbar.js
@@ -0,0 +1,36 @@
+;(function($){
+	$.fn.msgbar = function(options) {
+		return $(this).each(function(){
+			if ( options == null || options.msg == null || options.msg == '' )
+			{
+				$(this).css('display', 'none');
+				return;
+			}
+			
+			var settings = $.extend({}, $.fn.msgbar.defaults, options);
+			$(this).html('<span>' + settings.msg + '</span>');
+			
+			var span = $(this).children('span:eq(0)');
+			var width = span.outerWidth() + settings.corner * 2;
+			$(this).width(width < 200 ? 200 : width);
+			
+			$(this).css('background-color', settings.backColor );	
+//			$(this).css('width',  + 'px' );	
+//			$(this).css('margin', '0 auto');	
+			$(this).css('padding', '5px 0 5px');	
+//			$(this).css('height', '15px');
+//			$(this).css('line-height', '15px');
+			$(this).css('text-align', 'center');
+			$(this).css('margin', '5px auto 5px');	
+			$(this).css('font-weight', 'bold');	   			
+			$(this).corner(settings.corner);
+		});
+	};
+	
+	$.fn.msgbar.defaults = {
+		backColor : 'yellow', 
+		corner : 5,
+		msg : ''
+	};
+
+})(jQuery);
