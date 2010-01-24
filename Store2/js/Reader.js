b89c075bbb8a912251312cdc9a1505d9d2159feb
com.chinaos.snaill <com.chinaos.snaill@346b722a-c5ff-11de-8613-b9ccef3c78c6>
2010-01-24 07:55:03 +0000

    添加Reader网页及js，未实现

---
+++ Store2/js/Reader.js
@@ -0,0 +1,18 @@
+﻿/*
+ * Jeebook store 2.0
+ * Copyright(c) 2008-2009, Jeebook.com
+ * snaill@jeebook.com
+ * 
+ * http://www.jeebook.com
+ */
+
+$(document).ready(function(){
+	//
+	$("#reader").xslt("data/upload/A/_test.jb/index.xml", "xslt/default/index.xslt");
+
+	// end init
+	setTimeout(function(){
+		$('#loading').remove();
+		$('#loading-mask').fadeOut({remove:true});
+	}, 250);
+});
